8b569a02894eb7bf0927439abdb24e8df8d42230
Petr Marchanka <pmarchanka@i-deasoft.com>
2021-07-21 14:26:32 +0300

    test

--- src/dataset/bagpipes.ts
+++ src/dataset/bagpipes.ts
@@ -1,31 +1,42 @@
-import { Modes } from '../interfaces';
-import { BagpipeType, Bagpipes } from '../utils/bagpipesUtils';
+import { Modes } from "../interfaces";
+import { BagpipeType, Bagpipes } from "../utils/bagpipesUtils";
+console.log("test");
 
 export const bagpipes: BagpipeType[] = [
   {
     mode: Modes.Mixolidian,
     holesCount: 8,
-    name: 'Belarussian Duda - Mixolidian',
+    name: "Belarussian Duda - Mixolidian",
     type: Bagpipes.BelarussianDuda,
   },
   {
     mode: Modes.Dorian,
     holesCount: 8,
-    name: 'Belarussian Duda - Dorian',
+    name: "Belarussian Duda - Dorian",
     type: Bagpipes.BelarussianDuda,
   },
   {
     mode: Modes.Eolian,
     holesCount: 8,
-    name: 'Belarussian Duda - Minor',
+    name: "Belarussian Duda - Minor",
     type: Bagpipes.BelarussianDuda,
   },
   {
     mode: Modes.Ionian,
     holesCount: 8,
-    name: 'Belarussian Duda - Major',
+    name: "Belarussian Duda - Major",
     type: Bagpipes.BelarussianDuda,
   },
-  { mode: Modes.Ionian, holesCount: 8, name: 'Spanish Gaita', type: Bagpipes.SpanishGaita },
-  { mode: Modes.Eolian, holesCount: 8, name: 'Deutsch Dudelsack', type: Bagpipes.DeutschDudelsack },
+  {
+    mode: Modes.Ionian,
+    holesCount: 8,
+    name: "Spanish Gaita",
+    type: Bagpipes.SpanishGaita,
+  },
+  {
+    mode: Modes.Eolian,
+    holesCount: 8,
+    name: "Deutsch Dudelsack",
+    type: Bagpipes.DeutschDudelsack,
+  },
 ];
