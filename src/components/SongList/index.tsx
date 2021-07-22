610f59a4b12fbf0676c744434534b551d3ed8c67
Petr Marchanka <pmarchanka@i-deasoft.com>
2021-07-22 11:49:44 +0300

    fix song list width

--- src/components/SongList/index.tsx
+++ src/components/SongList/index.tsx
@@ -22,7 +22,7 @@ const useStyles = makeStyles((theme: Theme) =>
     container: {},
     formControl: {
       margin: theme.spacing(1),
-      minWidth: 120,
+      minWidth: 300,
     },
   })
 );
