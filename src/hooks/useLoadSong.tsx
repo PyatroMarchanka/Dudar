2e098ab081bbb639acfa8621772095377c43d058
Petr Marchanka <pmarchanka@i-deasoft.com>
2022-10-17 09:04:02 +0300

    add check to prevent unexisting songs loading

--- src/hooks/useLoadSong.tsx
+++ src/hooks/useLoadSong.tsx
@@ -5,17 +5,31 @@ import { fixMidiDataOctaves } from "../utils/midiUtils";
 
 export const useLoadSong = () => {
   const {
-    state: { activeSong, genreList },
+    state: { activeSong, genreList, allLists },
     setMidi,
     setMidiData,
   } = useContext(store);
 
   const loadMidiSong = async (fileName: string) => {
     try {
+      if (
+        Object.keys(allLists).length &&
+        genreList &&
+        !allLists[genreList].includes(fileName)
+      ) {
+        console.log(
+          `No song with this path in list \n ${genreList}/${fileName}`
+        );
+        return;
+      }
+
+      console.log(`Song exists in list \n ${genreList}/${fileName}`);
+
       console.log(`LoadMidiSong midi/${genreList}/${fileName}`);
       const file = await fetch(`midi/${genreList}/${fileName}`);
       const buffer = await file.arrayBuffer();
       const midi = new Midi(buffer);
+      console.log("midi", midi);
       setMidiData(fixMidiDataOctaves(midi));
       setMidi(buffer);
     } catch (error) {
@@ -27,5 +41,5 @@ export const useLoadSong = () => {
     if (activeSong) {
       loadMidiSong(activeSong);
     }
-  }, [activeSong, genreList]);
+  }, [activeSong, genreList, allLists]);
 };
