eefa6a4eb76accae365570e2e96fd63196c19e24
Petr Marchanka <pmarchanka@i-deasoft.com>
2022-10-17 08:10:55 +0300

    refactor fixMidiDataOctaves

--- src/utils/midiUtils.ts
+++ src/utils/midiUtils.ts
@@ -75,17 +75,11 @@ export const fixMidiDataOctaves = (midiData: Midi): Midi => {
     .map((e) => +e)
     .sort((a: any, b: any) => a - b);
 
-  if (octaves[0] !== 4 && octaves[1] !== 5) {
-    midiData.tracks[0].notes = notes.map((note) => {
-      if (octaves[0] < 4) {
-        note.octave += 1;
-        return note;
-      } else {
-        note.octave -= 1;
-        return note;
-      }
-    });
-  }
+  midiData.tracks[0].notes = notes.map((note) => {
+    note.octave += 4 - octaves[0];
+
+    return note;
+  });
 
   return midiData;
 };
