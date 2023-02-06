7582a22b46812030453872ef22c804f79a34a914
Piatro Marchanka <pmarchanka@gmail.com>
2023-02-06 20:03:04 +0100

    fix lint error

--- src/utils/drawUtils/drawBagpipe.ts
+++ src/utils/drawUtils/drawBagpipe.ts
@@ -8,11 +8,12 @@ export const drawBagpipe = (
   const bagpipeImage = bagpipes[bagpipeType].images.mainPipe;
   const imagesProperties = bagpipes[bagpipeType].imagesProperties;
 
-  ctx.drawImage(
-    bagpipeImage,
-    imagesProperties.main_pipe.leftMargin,
-    imagesProperties.main_pipe.topMargin,
-    imagesProperties.main_pipe.width,
-    imagesProperties.main_pipe.heigth
-  );
+  bagpipeImage &&
+    ctx.drawImage(
+      bagpipeImage,
+      imagesProperties.main_pipe.leftMargin,
+      imagesProperties.main_pipe.topMargin,
+      imagesProperties.main_pipe.width,
+      imagesProperties.main_pipe.heigth
+    );
 };
