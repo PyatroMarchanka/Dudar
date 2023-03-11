7176e019931755e153f888f8f010ed0651d1041f
Piatro Marchanka <pmarchanka@gmail.com>
2023-03-11 15:12:21 +0100

    comment initial script

--- load.js
+++ load.js
@@ -1,56 +1,56 @@
-console.log("Make MIDI Catalog...");
+console.log("Catalog making was skipped...");
 
-const folders = [
-  { path: "./public/midi/belarusian", label: "Belarusian" },
-  { path: "./public/midi/irish", label: "irish" },
-  // { path: "./public/midi/medieval", label: "medieval" },
-  // { path: "./public/midi/schotland", label: "schotland" },
-];
-const fs = require("fs");
+// const folders = [
+//   { path: "./public/midi/belarusian", label: "Belarusian" },
+//   { path: "./public/midi/irish", label: "irish" },
+//   // { path: "./public/midi/medieval", label: "medieval" },
+//   // { path: "./public/midi/schotland", label: "schotland" },
+// ];
+// const fs = require("fs");
 
-const checkSong = (song) => {
-  if (
-    !song.name ||
-    !song.type ||
-    !song.bagpipesToPlay ||
-    !song.bagpipesToPlay.length ||
-    !song.bagpipesToPlay[0] ||
-    !song.timeSignature ||
-    !song.pathName
-  ) {
-    console.error("Caugth an error parsing song name: ", song.pathName, song);
-  }
-};
+// const checkSong = (song) => {
+//   if (
+//     !song.name ||
+//     !song.type ||
+//     !song.bagpipesToPlay ||
+//     !song.bagpipesToPlay.length ||
+//     !song.bagpipesToPlay[0] ||
+//     !song.timeSignature ||
+//     !song.pathName
+//   ) {
+//     console.error("Caugth an error parsing song name: ", song.pathName, song);
+//   }
+// };
 
-const songs = [];
-folders.forEach((folder) => {
-  fs.readdirSync(folder.path).forEach((file) => {
-    if (file.includes(".mid")) {
-      const song = {};
-      const props = file.split("|");
-      song.name = props[0];
-      song.type = props[1];
-      song.bagpipesToPlay = props[2].split(",");
-      song.timeSignature = props[3];
-      song.pathName = file;
+// const songs = [];
+// folders.forEach((folder) => {
+//   fs.readdirSync(folder.path).forEach((file) => {
+//     if (file.includes(".mid")) {
+//       const song = {};
+//       const props = file.split("|");
+//       song.name = props[0];
+//       song.type = props[1];
+//       song.bagpipesToPlay = props[2].split(",");
+//       song.timeSignature = props[3];
+//       song.pathName = file;
 
-      checkSong(song);
+//       checkSong(song);
 
-      songs.push(song);
-    }
-  });
-});
+//       songs.push(song);
+//     }
+//   });
+// });
 
-fs.writeFile(
-  "./public/midi/list.json",
-  JSON.stringify(songs),
-  "utf8",
-  function (err) {
-    if (err) {
-      console.log("An error occured while writing JSON Object to File.");
-      return console.log(err);
-    }
+// fs.writeFile(
+//   "./public/midi/list.json",
+//   JSON.stringify(songs),
+//   "utf8",
+//   function (err) {
+//     if (err) {
+//       console.log("An error occured while writing JSON Object to File.");
+//       return console.log(err);
+//     }
 
-    console.log("MIDI Catalog file has been saved in list.json.");
-  }
-);
+//     console.log("MIDI Catalog file has been saved in list.json.");
+//   }
+// );
