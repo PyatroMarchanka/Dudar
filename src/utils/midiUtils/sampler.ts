de6c6f3aa5553f50d8d8bd33022d01ce2b676e64
karotkavichy <karotkavichy@gmail.com>
2023-09-27 13:01:22 +0200

    add d#2 sample

--- src/utils/midiUtils/sampler.ts
+++ src/utils/midiUtils/sampler.ts
@@ -3,6 +3,7 @@ import * as Tone from "tone";
 const sampler = new Tone.Sampler({
   urls: {
     A3: "samples/A2.mp3",
+    "D#3": "samples/D#2.mp3",
     A4: "samples/A3.mp3",
     C5: "samples/C4.mp3",
     E5: "samples/E4.mp3",
