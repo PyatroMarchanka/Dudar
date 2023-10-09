2bbb847b2a97f8ec442a42aacb3f349c1697d6e4
karotkavichy <karotkavichy@gmail.com>
2023-10-09 18:55:11 +0200

    add timeout for spinner

--- src/components/global/BackdropSpinner.tsx
+++ src/components/global/BackdropSpinner.tsx
@@ -1,6 +1,7 @@
 import { Dialog, makeStyles } from "@material-ui/core";
 import { mainColors } from "../../utils/theme";
 import { RotateSpinner } from "react-spinners-kit";
+import { useEffect, useState } from "react";
 
 interface Props {
   isOpen: boolean;
@@ -14,9 +15,22 @@ const useStyles = makeStyles(() => ({
 }));
 
 export const BackdropSpinner = ({ isOpen }: Props) => {
+  const [open, setOpen] = useState(isOpen);
+
   const classes = useStyles();
+
+  useEffect(() => {
+    if (isOpen) {
+      setOpen(true);
+    } else {
+      setTimeout(() => {
+        setOpen(false);
+      }, 500);
+    }
+  }, [isOpen]);
+
   return (
-    <Dialog open={isOpen} classes={classes} disableEscapeKeyDown>
+    <Dialog open={open} classes={classes} disableEscapeKeyDown>
       <RotateSpinner color={mainColors.darkestRed} size={150} />
     </Dialog>
   );
