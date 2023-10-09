214e12807d9e723e941c43b79def57d5c43c97aa
karotkavichy <karotkavichy@gmail.com>
2023-10-09 18:59:54 +0200

    remove timeout

--- src/components/global/BackdropSpinner.tsx
+++ src/components/global/BackdropSpinner.tsx
@@ -1,7 +1,6 @@
 import { Dialog, makeStyles } from "@material-ui/core";
 import { mainColors } from "../../utils/theme";
 import { RotateSpinner } from "react-spinners-kit";
-import { useEffect, useState } from "react";
 
 interface Props {
   isOpen: boolean;
@@ -15,22 +14,9 @@ const useStyles = makeStyles(() => ({
 }));
 
 export const BackdropSpinner = ({ isOpen }: Props) => {
-  const [open, setOpen] = useState(isOpen);
-
   const classes = useStyles();
-
-  useEffect(() => {
-    if (isOpen) {
-      setOpen(true);
-    } else {
-      setTimeout(() => {
-        setOpen(false);
-      }, 500);
-    }
-  }, [isOpen]);
-
   return (
-    <Dialog open={open} classes={classes} disableEscapeKeyDown>
+    <Dialog open={isOpen} classes={classes} disableEscapeKeyDown>
       <RotateSpinner color={mainColors.darkestRed} size={150} />
     </Dialog>
   );
