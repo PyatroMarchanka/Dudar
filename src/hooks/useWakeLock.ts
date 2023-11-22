8d502ce2fb6632a17cdd4ab07f4f4e144a5b3a33
karotkavichy <karotkavichy@gmail.com>
2023-11-22 18:16:22 +0100

    fix lint error

--- src/hooks/useWakeLock.ts
+++ src/hooks/useWakeLock.ts
@@ -5,7 +5,7 @@ export const useWakeLock = () => {
     if ("wakeLock" in window.navigator) {
       let screenLock;
       try {
-        screenLock = await navigator.wakeLock.request("screen");
+        screenLock = await (navigator as any).wakeLock.request("screen");
       } catch (err) {
         console.log(err);
       }
