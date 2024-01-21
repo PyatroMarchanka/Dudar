a432e62a6b17c393f09272518873d998f9dd0737
karotkavichy <karotkavichy@gmail.com>
2024-01-21 23:43:18 +0100

    remove prevent dfault

--- src/hooks/useLongPress.ts
+++ src/hooks/useLongPress.ts
@@ -1,9 +1,9 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React from 'react';
+import { useState, useEffect } from 'react';
 
 export function useLongPress(
   callback: (e: React.MouseEvent<HTMLButtonElement>) => void = () => {},
-  ms = 300
+  ms = 300,
 ) {
   const [startLongPress, setStartLongPress] = useState(false);
   const [event, setEvent] = useState<any | null>(null);
@@ -24,26 +24,20 @@ export function useLongPress(
   return {
     onMouseDown: (e: React.MouseEvent<HTMLButtonElement>) => {
       e.persist();
-      e.preventDefault();
       setEvent(e);
       setStartLongPress(true);
     },
-    onMouseUp: (e: React.MouseEvent<HTMLButtonElement>) => {
-      e.preventDefault();
-      setStartLongPress(false);
-    },
+    onMouseUp: () => setStartLongPress(false),
     onMouseLeave: () => {
       setEvent(null);
       setStartLongPress(false);
     },
     onTouchStart: (e: React.TouchEvent<HTMLButtonElement>) => {
       e.persist();
-      e.preventDefault();
       setEvent(e);
       setStartLongPress(true);
     },
-    onTouchEnd: (e: React.MouseEvent<HTMLButtonElement>) => {
-      e.preventDefault();
+    onTouchEnd: () => {
       setEvent(null);
       setStartLongPress(false);
     },
