04f3010c65e567f2eb30ea87161cb4a27e2db0f0
karotkavichy <karotkavichy@gmail.com>
2024-02-01 23:30:53 +0100

    add fallback to first dong

--- src/hooks/useLoadSong.tsx
+++ src/hooks/useLoadSong.tsx
@@ -42,6 +42,7 @@ export const useLoadSong = () => {
       setIsSongLoading(false);
     } catch (error) {
       console.log(error);
+      listsByBagpipe && setActiveSong(listsByBagpipe[bagpipeType][0]);
     }
   };
 
