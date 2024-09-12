973bdfc2e2dd59a615590aadd6b4dfe8d6e937e9
karotkavichy <karotkavichy@gmail.com>
2024-09-12 10:42:51 +0200

    update links

--- src/api/links.ts
+++ src/api/links.ts
@@ -1,14 +1,6 @@
 export const links = {
-  login: `${
-    process.env.REACT_APP_LOCAL_BACKEND_URL || process.env.REACT_APP_BACKEND_URL
-  }/v1/auth/google`,
-  logout: `${
-    process.env.REACT_APP_LOCAL_BACKEND_URL || process.env.REACT_APP_BACKEND_URL
-  }/v1/auth/logout`,
-  profile: `${
-    process.env.REACT_APP_LOCAL_BACKEND_URL || process.env.REACT_APP_BACKEND_URL
-  }/v1/profile`,
-  updateSettings: `${
-    process.env.REACT_APP_LOCAL_BACKEND_URL || process.env.REACT_APP_BACKEND_URL
-  }/v1/settings-update`,
+  login: `${process.env.REACT_APP_BACKEND_URL}/v1/auth/google`,
+  logout: `${process.env.REACT_APP_BACKEND_URL}/v1/auth/logout`,
+  profile: `${process.env.REACT_APP_BACKEND_URL}/v1/profile`,
+  updateSettings: `${process.env.REACT_APP_BACKEND_URL}/v1/settings-update`,
 };
