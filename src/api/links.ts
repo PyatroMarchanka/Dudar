2c44f9baf8a1e80d2df14946bf8cc402f5c68a2f
karotkavichy <karotkavichy@gmail.com>
2024-11-15 21:11:35 +0100

    rework routes

--- src/api/links.ts
+++ src/api/links.ts
@@ -1,6 +1,6 @@
 export const links = {
-  login: `${process.env.REACT_APP_BACKEND_URL}/v1/auth/google`,
-  loginServer: `${process.env.REACT_APP_BACKEND_URL}/v1/auth/some-auth`,
+  login: `${process.env.REACT_APP_BACKEND_URL}/v1/google`,
+  loginServer: `${process.env.REACT_APP_BACKEND_URL}/v1/some-auth`,
   logout: `${process.env.REACT_APP_BACKEND_URL}/v1/auth/logout`,
   profile: `${process.env.REACT_APP_BACKEND_URL}/v1/profile`,
   updateSettings: `${process.env.REACT_APP_BACKEND_URL}/v1/settings-update`,
