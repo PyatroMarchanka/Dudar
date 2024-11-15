3244b95f91abb968391cd5acb5ab1ac45ff066fe
karotkavichy <karotkavichy@gmail.com>
2024-11-15 20:55:41 +0100

    add test

--- src/api/links.ts
+++ src/api/links.ts
@@ -1,6 +1,6 @@
 export const links = {
   login: `${process.env.REACT_APP_BACKEND_URL}/v1/auth/google`,
-  loginServer: `${process.env.REACT_APP_BACKEND_URL}/v1/auth/google-auth`,
+  loginServer: `${process.env.REACT_APP_BACKEND_URL}/v1/auth/some-auth`,
   logout: `${process.env.REACT_APP_BACKEND_URL}/v1/auth/logout`,
   profile: `${process.env.REACT_APP_BACKEND_URL}/v1/profile`,
   updateSettings: `${process.env.REACT_APP_BACKEND_URL}/v1/settings-update`,
